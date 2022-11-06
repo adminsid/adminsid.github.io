f7524237ecd9cd58de8699a242c827ffc61416ab
adminSid <siddhartha.lama3@gmail.com>
2022-11-06 14:49:37 -0500

    set up js file for barchart.

--- labs/chart/main.js
+++ labs/chart/main.js
@@ -1,20 +1,63 @@
 var fruit = [
-	{name:"Strawberry", quantity:15, color:"red"},
-	{name:"Peach", quantity:10, color:"orange"},
-	{name:"Banana", quantity:25, color:"yellow"},
-	{name:"Pear", quantity:10, color:"green"},
-	{name:"Grape", quantity:20, color:"black"},
-
+	{ name: "Strawberry", quantity: 15, color: "red" },
+	{ name: "Peach", quantity: 10, color: "orange" },
+	{ name: "Banana", quantity: 25, color: "yellow" },
+	{ name: "Pear", quantity: 10, color: "green" },
+	{ name: "Grape", quantity: 20, color: "black" },
 ];
 
-class BarChart {
-	constructor(options) {
-	  this.options = options;
-	  this.canvas = options.canvas;
-	  this.ctx = this.canvas.getContext("2d");
-	  this.colors = options.colors;
-	  this.titleOptions = options.titleOptions;
-	  this.maxValue = Math.max(...Object.values(this.options.data));
+var canvas = document.getElementById("myCanvas");
+canvas.setAttribute('width', window.innerWidth);
+canvas.setAttribute('height', window.innerHeight - 50);
+
+var c = canvas.getContext("2d");
+
+console.log(fruit);
+
+function drawLine(c, startX, startY, endX, endY,color){
+    c.save();
+    c.strokeStyle = color;
+    c.beginPath();
+    c.moveTo(startX,startY);
+    c.lineTo(endX,endY);
+    c.stroke();
+    c.restore();
+}
+
+function drawBar(c, upperLeftCornerX, upperLeftCornerY, width, height,color){
+    c.save();
+    c.fillStyle=color;
+    c.fillRect(upperLeftCornerX,upperLeftCornerY,width,height);
+    c.restore();
+}
+
+class BarChart{
+	constructor (options) {
+		
 	}
 
+	drawGridLines() {
+
+	}
+
+	drawBars() {
+		drawBar(
+			c,
+			200,
+			200,
+			50,
+			100,
+			fruit[0].color
+		)
+	}
+
+	draw() {
+		this.drawGridLines();
+		this.drawBars();
+	}
 }
+
+var myBarchart = new BarChart({
+
+});
+myBarchart.draw()
