9ce63271c2d3b46012769131458d3005f0c931fa
adminSid <siddhartha.lama3@gmail.com>
2022-11-06 19:36:47 -0500

    added bars

--- labs/chart/main.js
+++ labs/chart/main.js
@@ -14,28 +14,29 @@ var c = canvas.getContext("2d");
 
 console.log(fruit);
 
-function drawLine(c, startX, startY, endX, endY,color){
-    c.save();
-    c.strokeStyle = color;
-    c.beginPath();
-    c.moveTo(startX,startY);
-    c.lineTo(endX,endY);
-    c.stroke();
-    c.restore();
+function drawLine(c, startX, startY, endX, endY, color) {
+	c.save();
+	c.strokeStyle = color;
+	c.beginPath();
+	c.moveTo(startX, startY);
+	c.lineTo(endX, endY);
+	c.stroke();
+	c.restore();
 }
 
-function drawBar(c, upperLeftCornerX, upperLeftCornerY, width, height,color){
-    c.save();
-    c.fillStyle=color;
-    c.fillRect(upperLeftCornerX,upperLeftCornerY,width,height);
-    c.restore();
+function drawBar(c, upperLeftCornerX, upperLeftCornerY, width, height, color) {
+	c.save();
+	c.fillStyle = color;
+	c.fillRect(upperLeftCornerX, upperLeftCornerY, width, height);
+	c.restore();
 }
 
-class BarChart{
-	constructor (options) {
+class BarChart {
+	constructor(options) {
 		this.options = options;
 		this.canvas = options.myCanvas;
 		this.c = this.canvas.getContext("2d");
+		this.colors = options.colors;
 		this.data = options.data;
 		this.maxValue = 25;
 	}
@@ -45,7 +46,7 @@ class BarChart{
 		var canvasActualWidth = this.canvas.width - this.options.padding * 2;
 
 		var gridValue = 0;
-		while(gridValue <= this.maxValue) {
+		while (gridValue <= this.maxValue) {
 			var gridX = canvasActualHeight * (1 - gridValue / this.maxValue) + this.options.padding;
 			drawLine(
 				this.c,
@@ -58,34 +59,46 @@ class BarChart{
 			drawLine(
 				this.c,
 				15,
-				this.options.padding/2,
+				this.options.padding / 2,
 				15,
-				gridX + this.options.padding/2,
+				gridX + this.options.padding / 2,
 				this.options.gridColor
-			  );
-		 
-			  // Writing grid markers
-			  this.c.save();
-			  this.c.fillStyle = this.options.gridColor;
-			  this.c.textBaseline = "bottom";
-			  this.c.font = "bold 10px Arial";
-			  this.c.fillText(gridValue, 0, gridX - 2);
-			  this.c.restore();
-		 
+			);
+
+			// Writing grid markers
+			this.c.save();
+			this.c.fillStyle = this.options.gridColor;
+			this.c.textBaseline = "bottom";
+			this.c.font = "bold 10px Arial";
+			this.c.fillText(gridValue, 0, gridX - 2);
+			this.c.restore();
+
 			gridValue += this.options.gridScale;
 		}
-		
+
 	}
 
 	drawBars() {
-		drawBar(
-			c,
-			100, // x coordinate
-			200, // y coordinate
-			15, //quanitity
-			50, // size of the bar
-			fruit[0].color
-		)
+		var canvasActualHeight = this.canvas.height - this.options.padding * 2;
+		var canvasActualWidth = this.canvas.width - this.options.padding * 2;
+		var barIndex = 0;
+		var numberOfBars = Object.keys(this.options.data).length;
+		var barSize = canvasActualWidth / numberOfBars;
+		var values = Object.values(this.options.data);
+		console.log(values);
+		for(let val of values) {
+			var barHeight = Math.round((canvasActualHeight * val) / this.maxValue)
+			drawBar(
+				this.c,
+				this.options.padding + barIndex * barSize,
+				this.canvas.height - barHeight - this.options.padding,
+				barSize,
+				barHeight,
+				this.colors[barIndex % this.colors.length]
+			  );
+
+			barIndex++;
+		}
 	}
 
 	draw() {
@@ -99,7 +112,14 @@ var myBarchart = new BarChart({
 	padding: 40,
 	gridScale: 5,
 	gridColor: "black",
-	data: fruit
+	data: {
+		"Strawberry" : 15,
+		"Peach" : 10,
+		"Banana":25,
+		"Pear":10,
+		"Grape":20,
+	},
+	colors: ["red","orange","yellow","green","black"],
 });
 
 myBarchart.draw()
