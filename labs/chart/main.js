dbf58785cba04c84bb1b9aad15f5177914e7a84d
adminSid <siddhartha.lama3@gmail.com>
2022-11-06 19:08:51 -0500

    added grid

--- labs/chart/main.js
+++ labs/chart/main.js
@@ -33,20 +33,57 @@ function drawBar(c, upperLeftCornerX, upperLeftCornerY, width, height,color){
 
 class BarChart{
 	constructor (options) {
-		
+		this.options = options;
+		this.canvas = options.myCanvas;
+		this.c = this.canvas.getContext("2d");
+		this.data = options.data;
+		this.maxValue = 25;
 	}
 
 	drawGridLines() {
+		var canvasActualHeight = this.canvas.height - this.options.padding * 2;
+		var canvasActualWidth = this.canvas.width - this.options.padding * 2;
 
+		var gridValue = 0;
+		while(gridValue <= this.maxValue) {
+			var gridX = canvasActualHeight * (1 - gridValue / this.maxValue) + this.options.padding;
+			drawLine(
+				this.c,
+				0,
+				gridX,
+				this.canvas.width,
+				gridX,
+				this.options.gridColor
+			);
+			drawLine(
+				this.c,
+				15,
+				this.options.padding/2,
+				15,
+				gridX + this.options.padding/2,
+				this.options.gridColor
+			  );
+		 
+			  // Writing grid markers
+			  this.c.save();
+			  this.c.fillStyle = this.options.gridColor;
+			  this.c.textBaseline = "bottom";
+			  this.c.font = "bold 10px Arial";
+			  this.c.fillText(gridValue, 0, gridX - 2);
+			  this.c.restore();
+		 
+			gridValue += this.options.gridScale;
+		}
+		
 	}
 
 	drawBars() {
 		drawBar(
 			c,
-			200,
-			200,
-			50,
-			100,
+			100, // x coordinate
+			200, // y coordinate
+			15, //quanitity
+			50, // size of the bar
 			fruit[0].color
 		)
 	}
@@ -58,6 +95,11 @@ class BarChart{
 }
 
 var myBarchart = new BarChart({
-
+	myCanvas: canvas,
+	padding: 40,
+	gridScale: 5,
+	gridColor: "black",
+	data: fruit
 });
+
 myBarchart.draw()
