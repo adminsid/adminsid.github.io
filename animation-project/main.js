a4c6f387fecba0dacfeaaa2920e801a449cf64dd
adminSid <siddhartha.lama3@gmail.com>
2022-11-14 21:33:04 -0500

    adjusted the  frame.

--- animation-project/main.js
+++ animation-project/main.js
@@ -5,10 +5,13 @@ const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
 playerImage.src = 'spritesheet.png';
+const spriteWidth = 268;
+const spriteHeight = 209;
 
 function animate() {
     c.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    c.drawImage(playerImage, 0, 0);
+    // c.drawImage(image, sx, sw, sh, dx, dy, dh);
+    c.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
     requestAnimationFrame(animate);
 };
 animate();
