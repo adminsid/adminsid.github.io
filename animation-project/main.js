4251557621ca4770894f46c1895b122ccdfcdf36
adminSid <siddhartha.lama3@gmail.com>
2022-11-14 21:49:48 -0500

    animated the frame and added staggerframes to 2.

--- animation-project/main.js
+++ animation-project/main.js
@@ -7,11 +7,21 @@ const playerImage = new Image();
 playerImage.src = 'spritesheet.png';
 const spriteWidth = 268;
 const spriteHeight = 209;
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 0;
+const staggerFrames  = 2;
 
 function animate() {
     c.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
     // c.drawImage(image, sx, sw, sh, dx, dy, dh);
-    c.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    c.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    if (gameFrame % staggerFrames == 0) {
+        if(frameX < 14) frameX++;
+        else frameX = 0;
+    }
+
+    gameFrame++;
     requestAnimationFrame(animate);
 };
 animate();
